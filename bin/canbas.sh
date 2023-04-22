25a52fbb09d8f350004eea6ac71a0bcd7e2a3c12
nm-jp <118420278+nm-jp@users.noreply.github.com>
2023-04-22 19:31:45 +0900

    add a new option for changing the component character

--- bin/canbas.sh
+++ bin/canbas.sh
@@ -8,12 +8,13 @@ set -eu
 print_usage_and_exit () {
   cat <<-USAGE 1>&2
 	Usage   : ${0##*/} -r<行数> -c<列数>
-	Options : 
+	Options : -p<構成文字>
 
-	空文字（□）を矩形状に並べて出力する。
+	文字を矩形状に並べて出力する。
 
-	-rオプションで行数を指定できる。
-	-cオプションで列数を指定できる。
+	-rオプションで行数を指定する。
+	-cオプションで列数を指定する。
+	-pオプションで構成文字を指定できる。デフォルトは"□"。
 	USAGE
   exit 1
 }
@@ -25,6 +26,7 @@ print_usage_and_exit () {
 # 変数を初期化
 opt_r=''
 opt_c=''
+opt_p='□'
 
 # 引数をパース
 i=1
@@ -34,6 +36,7 @@ do
     -h|--help|--version) print_usage_and_exit ;;    
     -r*)                 opt_r=${arg#-r}      ;;
     -c*)                 opt_c=${arg#-c}      ;;
+    -p*)                 opt_p=${arg#-p}      ;;
     *)
       echo "${0##*/}: invalid args" 1>&2
       exit 11
@@ -55,9 +58,16 @@ if ! printf '%s\n' "$opt_c" | grep -Eq '^[0-9]+$'; then
   exit 31
 fi
 
+# 有効な１文字であるか判定
+if ! printf '%s\n' "$opt_p" | grep -Eq '^.$'; then
+  echo "${0##*/}: \"$opt_p\" invalid character" 1>&2
+  exit 41
+fi
+
 # パラメータを決定
 height=$opt_r
 width=$opt_c
+tchar=$opt_p
 
 ######################################################################
 # 本体処理
@@ -67,10 +77,10 @@ gawk '
 BEGIN {
   height = '"${height}"';
   width  = '"${width}"';
-  c      = "□";
+  tchar  = "'"${tchar}"'";
 
   for (i = 1; i <= height; i++) {
-    for (j = 1; j <= width; j++) { printf "%s", c; }
+    for (j = 1; j <= width; j++) { printf "%s", tchar; }
     print "";
   }
 }
